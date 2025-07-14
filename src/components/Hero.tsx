a7c871d24c231adeb621935d73bb08e8f36ab367
Vineet Saini <vineetsainiii007@gmail.com>
2025-07-14 22:26:53 +0530

    more functionalities

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,6 +3,7 @@ import { ArrowRight, MessageCircle, Instagram, Mail, ChevronUp } from 'lucide-re
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { CountUp } from '@/utils/CountUp';
+import RotatingText from '@/utils/RotatingText';
 gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
@@ -113,7 +114,20 @@ const Hero = () => {
             <h1 className="font-apple text-5xl md:text-7xl font-bold text-white leading-tight">
               When We Build
               <span className="font-apple block text-transparent bg-clip-text bg-white">
-                Others Redesign
+                Others <RotatingText
+  texts={['Redesign', 'Panic', 'Copy']}
+  mainClassName="px-2 sm:px-2 md:px-3 bg-transparent-300 text-white overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+  staggerFrom={"last"}
+  initial={{ y: "100%" }}
+  animate={{ y: 0 }}
+  exit={{ y: "-120%" }}
+  staggerDuration={0.025}
+  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+  transition={{ type: "spring", damping: 30, stiffness: 400 }}
+  rotationInterval={2000}
+  auto ={true}
+  loop={true} 
+/>
               </span>
             </h1>
             
